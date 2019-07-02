7536259fbc864b2b53b7e679b17a201bfc0343d7
jjur <jjur@pakrapot.sk>
2019-07-02 20:11:24 +0200

    Update requirements.sh from editor

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,5 @@
 # If bash command fails, build should error out
 set -e
-pip3 install --upgrade pip
 # Install specific package versions with pip
 # Check the latest version using https://pypi.org/search
 
@@ -10,10 +9,9 @@ pip3 install matplotlib
 # pip3 install numpy==1.15.0
 # pip3 install seaborn==0.9.0
 # pip3 install bokeh==0.13.0
-pip3 install random
 
 # Include datasets in the image
 
-# DATADIR=/usr/local/share/datasets
-# mkdir -p $DATADIR
-# wget -O $DATADIR/iris.csv http://s3.amazonaws.com/assets.datacamp.com/staging/course_2406/datasets/iris.csv
+DATADIR=/usr/local/share/datasets
+ mkdir -p $DATADIR
+ wget -O $DATADIR/iris.csv http://s3.amazonaws.com/assets.datacamp.com/staging/course_2406/datasets/iris.csv
